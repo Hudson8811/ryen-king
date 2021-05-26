15e50d9d0e9097ea5fa94208d4330ac4d3c60c05
burdms <burdms@gmail.com>
2021-05-26 17:10:51 +0200

    fix: swiper conflict with main

--- build/js/main.js
+++ build/js/main.js
@@ -1302,33 +1302,32 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 
 })
-let swiper = new Swiper('.swiper-container', {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
+// let swiper = new Swiper('.swiper-container', {
+//     // Optional parameters
+//     direction: 'horizontal',
+//     loop: true,
 
-    // Navigation arrows
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
+//     // Navigation arrows
+//     navigation: {
+//         nextEl: '.swiper-button-next',
+//         prevEl: '.swiper-button-prev',
+//     },
 
-});
-
+// });
 
 let swiper2 = new Swiper('.swiper-container2', {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
-    slidesPerView: 'auto',
-    spaceBetween: 40,
-    // slidesPerGroup: 3,
-    // freeMode: true,
-    // Navigation arrows
-    navigation: {
-        nextEl: '.swiper-button-next2',
-        prevEl: '.swiper-button-prev2',
-    },
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  slidesPerView: 'auto',
+  spaceBetween: 40,
+  // slidesPerGroup: 3,
+  // freeMode: true,
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next2',
+    prevEl: '.swiper-button-prev2',
+  },
 });
 
 /* 
--- src/js/partials/swiper.js
+++ src/js/partials/swiper.js
@@ -1,28 +1,26 @@
-let swiper = new Swiper('.swiper-container', {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
+let swiper = new Swiper('.swiper-container1', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
 
-    // Navigation arrows
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
 });
 
-
 let swiper2 = new Swiper('.swiper-container2', {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
-    slidesPerView: 'auto',
-    spaceBetween: 40,
-    // slidesPerGroup: 3,
-    // freeMode: true,
-    // Navigation arrows
-    navigation: {
-        nextEl: '.swiper-button-next2',
-        prevEl: '.swiper-button-prev2',
-    },
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  slidesPerView: 'auto',
+  spaceBetween: 40,
+  // slidesPerGroup: 3,
+  // freeMode: true,
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next2',
+    prevEl: '.swiper-button-prev2',
+  },
 });
