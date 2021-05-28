f9e6bcefa80846b385a4a081bac13080b65f9d9e
burdms <burdms@gmail.com>
2021-05-28 19:07:15 +0200

    create: header js scroll to anchor, refactoring, eventListener (not ready yet)

--- src/js/partials/header.js
+++ src/js/partials/header.js
@@ -1,41 +1,53 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const burger = document.querySelector('.header__burger');
-    const menu = document.querySelector('.header__menu');
-    const menuExit = document.querySelector('.header__menu-exit');
-    const main = document.querySelector('.main');
-    const header = document.querySelector('.header');
+  const burger = document.querySelector('.header__burger');
+  const menu = document.querySelector('.header__menu');
+  const menuExit = document.querySelector('.header__menu-exit');
+  const main = document.querySelector('.main');
+  const header = document.querySelector('.header');
 
-    burger.addEventListener('click', function () {
-        menu.classList.add('active');
-    })
+  //   burger.addEventListener('click', function () {
+  //     menu.classList.add('active');
+  //   });
 
-    menuExit.addEventListener('click', function () {
-        menu.classList.remove('active');
-    })
+  //   menuExit.addEventListener('click', function () {
+  //     menu.classList.remove('active');
+  //   });
 
-    if (header.classList.contains("header-js")) {
-        window.addEventListener('scroll', () => {
-            let y = window.scrollY;
+  document.addEventListener('click', (event) => {
+    const target = event.target;
 
-            let headerHeight = header.offsetHeight;
-
-            if (y > headerHeight) {
-                main.style.paddingTop = headerHeight + 'px';
-                header.classList.add('is-fixed');
-                header.style.top = -headerHeight + "px";
-                header.style.transform = "translateY(100%)";
-            } else if (y === 0) {
-                header.classList.remove('is-fixed');
-                main.style.paddingTop = 0;
-                header.style.top = 0;
-                header.style.transform = "translateY(0%)";
-            }
-        })
-    }
-    function disabledPreloader() {
-        document.getElementById('preloader').style.display = 'none';
+    if (target.classList.contains('header__burger')) {
+      menu.classList.add('active');
     }
 
-})
+    if (
+      target.classList.contains('header__menu-exit') ||
+      target.closest('.header__menu-item')
+    ) {
+      menu.classList.remove('active');
+    }
+  });
 
+  if (header.classList.contains('header-js')) {
+    window.addEventListener('scroll', () => {
+      let y = window.scrollY;
 
+      let headerHeight = header.offsetHeight;
+
+      if (y > headerHeight) {
+        main.style.paddingTop = headerHeight + 'px';
+        header.classList.add('is-fixed');
+        header.style.top = -headerHeight + 'px';
+        header.style.transform = 'translateY(100%)';
+      } else if (y === 0) {
+        header.classList.remove('is-fixed');
+        main.style.paddingTop = 0;
+        header.style.top = 0;
+        header.style.transform = 'translateY(0%)';
+      }
+    });
+  }
+  function disabledPreloader() {
+    document.getElementById('preloader').style.display = 'none';
+  }
+});
