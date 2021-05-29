27e9b75a3e5230be51eabcd451036539c2506341
burdms <burdms@gmail.com>
2021-05-29 16:49:58 +0200

    create: set menu item active while in the corresponding section

--- src/js/partials/header.js
+++ src/js/partials/header.js
@@ -4,6 +4,7 @@ document.addEventListener('DOMContentLoaded', function () {
   const menuExit = document.querySelector('.header__menu-exit');
   const main = document.querySelector('.main');
   const header = document.querySelector('.header');
+  const menuLinks = document.querySelectorAll('.header__menu-link');
 
   //   burger.addEventListener('click', function () {
   //     menu.classList.add('active');
@@ -13,23 +14,39 @@ document.addEventListener('DOMContentLoaded', function () {
   //     menu.classList.remove('active');
   //   });
 
-  document.addEventListener('click', (event) => {
+  // Menu Handler
+  document.addEventListener('click', event => {
     const target = event.target;
 
     if (target.classList.contains('header__burger')) {
       menu.classList.add('active');
-    } else if (
-      target.classList.contains('header__menu-exit') ||
-      target.closest('.header__menu-item')
-    ) {
+    } else if (target.classList.contains('header__menu-exit') || target.closest('.header__menu-item')) {
       menu.classList.remove('active');
-    } else if (
-      menu.classList.contains('active') &&
-      !target.closest('.header__menu')
-    ) {
+    } else if (menu.classList.contains('active') && !target.closest('.header__menu')) {
       menu.classList.remove('active');
     }
   });
+
+  // Start of set menu item link active
+  if (location.hash === '') {
+    menuLinks[0].closest('.header__menu-item').classList.add('active');
+  } else {
+    setActiveLink();
+  }
+
+  function setActiveLink() {
+    menuLinks.forEach((item, index) => {
+      if (item.getAttribute('href') === location.hash) {
+        menuLinks[index].closest('.header__menu-item').classList.add('active');
+        console.log(menuLinks[index].closest('.header__menu-item').classList);
+      }
+    });
+  }
+
+  window.addEventListener('hashchange', () => {
+    setActiveLink();
+  });
+  // End of set menu item link active
 
   if (header.classList.contains('header-js')) {
     window.addEventListener('scroll', () => {
